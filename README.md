a20b5b9950e6c67e588f247602e57f4c63b0d6c4
io42630 <ivan@olexyn.com>
2022-06-22 23:34:32 +0200

    + format / doc

---
+++ README.md
@@ -0,0 +1,10 @@
+# Translate ReeBot
+
+### About
+* Uses DeepL for Telegram inline translations.
+
+
+### How To
+* enter API-keys into `keys-template.json`, then rename to `keys.json`.
+* `npm run build / start`
+* in Telegram : add bot > enable inline > /start > use via inline.
--- src/bot-adapter.ts
+++ src/bot-adapter.ts
@@ -1,10 +1,10 @@
-import TelegramBot from "node-telegram-bot-api";
-import { Options } from "request";
+import TelegramBot, { InlineQuery } from 'node-telegram-bot-api';
+import { Options } from 'request';
 
-const translate = require('./translate-adapter').translate
+const translate = require('./translate-adapter').translate;
 const keys = require('../keys.json');
 
-let request_options: Options = { url: "" };
+let request_options: Options = {url: ''};
 
 let options: TelegramBot.ConstructorOptions = {
     polling: true,
@@ -12,22 +12,19 @@ let options: TelegramBot.ConstructorOptions = {
 };
 const bot = new TelegramBot(keys.TRANSLATE_REEBOT, options);
 
-bot.on("inline_query", async msg => {
-    console.log(JSON.stringify(msg));
-    let results: Array<TelegramBot.InlineQueryResult> = [];
-    if (msg.query.length <= 1) return;
+bot.on('inline_query', async (msg: InlineQuery) => {
     if (msg.query.length > 1) {
-        let translated  = await translate(msg.query, 'de');
-        results = [
+        const translated = (await translate(msg.query, 'de')).text;
+        const results: Array<TelegramBot.InlineQueryResult> = [
             {
                 type: 'article',
                 id: 'hello_id',
-                title: translated.text,
+                title: translated,
                 input_message_content: {
-                    message_text: translated.text
+                    message_text: translated
                 }
             }
         ];
+        await bot.answerInlineQuery(msg.id, results);
     }
-    bot.answerInlineQuery(msg.id, results);
 });
