0ccfde456e21ec52727ef845ec2c1794b1543570
io42630 <ivan@olexyn.com>
2022-06-26 20:46:28 +0200

    + no need to parse en to en-US in srcLang

--- src/deepl-translate-adapter.ts
+++ src/deepl-translate-adapter.ts
@@ -3,7 +3,6 @@ import { Translator} from 'deepl-node';
 const translator = new Translator(require('../keys.json').DEEPL);
 
 export async function translate(text: string, srcLang:any, targetLang: any): Promise<string> {
-    srcLang = srcLang === 'en' ? 'en-US' : srcLang;
     targetLang = targetLang === 'en' ? 'en-US' : targetLang;
     return translator.translateText(text, srcLang, targetLang)
         .then((textResult => textResult.text));
